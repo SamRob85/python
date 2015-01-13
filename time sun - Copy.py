67e42493567e312be2d9ef1a3e36b6decdd44dd5
SamRob85 <smallburn85@gmail.com>
2015-01-13 18:40:49 +0000

    Program to find the distance the earth has traveled
    This is my first draft of the program

---
+++ time sun - Copy.py
@@ -0,0 +1,21 @@
+import math
+from datetime import datetime
+date=input("What date do you want from")
+d = datetime.now()
+d1 = datetime.strptime(date, '%d/%m/%Y')
+x=(d-d1).days
+pi=math.pi
+earthtosun=149597870700
+diameter=earthtosun*2
+circumference=pi*diameter
+degree=circumference/360
+totalangle=360
+daysinyear=365.256
+angleday=totalangle/daysinyear
+totalangle=x*angleday
+totaldistance=totalangle*degree
+print(str(totaldistance)+"m or "+str((totaldistance/1000))+"km")
+
+
+
+
